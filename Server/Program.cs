e25e77533c64bbabcfc3ebd8abc4007e17adcb29
mylena mel <mylenamsfarias@gmail.com>
2023-12-03 20:57:44 -0300

    feat: created listener

---
+++ Server/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] prefixes = 
+            { 
+                "http://localhost:8080/get/products/",
+                "http://localhost:8080/post/product/",
+                "http://localhost:8080/put/product/",
+                "http://localhost:8080/delete/produt/" 
+            };
+
+
+            HttpListener listener = new HttpListener();
+            
+            foreach(string prefix in prefixes)
+            {
+                listener.Prefixes.Add(prefix);
+            }
+
+            listener.Start();
+        }
+    }
+}
