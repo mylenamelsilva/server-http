38c7143dc376b7ddb1f692d47d1140e09f19e7fd
mylena mel <mylenamsfarias@gmail.com>
2023-12-04 19:01:07 -0300

    chore: created prodct model

---
+++ Server/Service/ProductService.cs
@@ -0,0 +1,17 @@
+﻿using Server.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Service
+{
+    public class ProductService
+    {
+        public List<ProductModel> ListAllProducts()
+        {
+
+        }
+    }
+}
