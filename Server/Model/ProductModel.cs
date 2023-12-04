038340bfa6814e9a9428d3daae540a96c13c3598
mylena mel <mylenamsfarias@gmail.com>
2023-12-04 19:04:41 -0300

    chore: created product model

---
+++ Server/Model/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Model
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Product { get; set; }
+        public double Price { get; set; }
+    }
+}
